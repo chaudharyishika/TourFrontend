2b6951f0d22c973a9453a19303f866854e533bf7
Ishika Chaudhary <120246547+chaudharyishika@users.noreply.github.com>
2025-01-05 13:18:17 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -74,7 +74,7 @@ function Home() {
   };
 
   return (
-    <div className="bg-gray-100 min-h-screen sm:pt-[10px] lg:pt-[300px] mt-[160rem] ">
+    <div className="bg-gray-100 min-h-screen sm:pt-[10px] lg:pt-[300px] mt-[170rem] ">
       <div className="py-8  sm:py-12 bg-white">
         <h1 className="text-4xl font-bold text-center text-indigo-600 mb-8">
           Welcome to Our Tour Packages
