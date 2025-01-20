80c32e1ecf758528afc152280d46895b3fa5f94c
Ishika Chaudhary <120246547+chaudharyishika@users.noreply.github.com>
2025-01-20 18:30:22 +0530

    Update Review.jsx

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -34,7 +34,7 @@ function ReviewSection() {
     try {
       const token = localStorage.getItem("token");
       const response = await axios.post(
-        "https://backsampl.onrender.com/api/review",
+        "https://backsampl.onrender.com/api/review/",
         formData,
         { headers: { Authorization: `Bearer ${token}` } }
       );
