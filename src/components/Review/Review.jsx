77f2fb8eb06961a534547a1655a99c2ba354eb44
Ishika Chaudhary <120246547+chaudharyishika@users.noreply.github.com>
2025-01-20 18:16:36 +0530

    Update Review.jsx

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -10,7 +10,7 @@ function ReviewSection() {
   useEffect(() => {
     const fetchReviews = async () => {
       try {
-        const response = await axios.get("https://backsampl.onrender.com/api/review/");
+        const response = await axios.post("https://backsampl.onrender.com/api/review/");
         
         setReviews(response.data.data);
       } catch (error) {
